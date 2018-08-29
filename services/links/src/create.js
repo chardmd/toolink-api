90fb59608b5781669335df00630bea53892b199b
Richard <richard.dimalanta@gmail.com>
2018-08-29 23:02:54 +1000

    scrap data from url

--- services/links/src/create.js
+++ services/links/src/create.js
@@ -1,28 +1,49 @@
-import uuid from "uuid";
-import * as dynamoDbLib from "../../../libs/dynamodb-lib";
+const urlParser = require("url");
+const got = require("got");
 import { success, failure } from "../../../libs/response-lib";
+
+const metascraper = require("metascraper")([
+  require("metascraper-author")(),
+  require("metascraper-date")(),
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-logo")(),
+  require("metascraper-clearbit-logo")(),
+  require("metascraper-publisher")(),
+  require("metascraper-title")(),
+  require("metascraper-url")(),
+  require("metascraper-logo-favicon")(),
+  require("metascraper-amazon")(),
+  require("metascraper-youtube")(),
+  require("metascraper-soundcloud")(),
+  require("metascraper-video-provider")()
+]);
 
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
-  const params = {
-    TableName: process.env.TABLE_NAME,
-    Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      linkId: uuid.v1(),
-      createdAt: Date.now()
-    }
-  };
-
-  try {
-    await dynamoDbLib.call("put", params);
-    callback(
+  const validUrl = urlParser.parse(data.url, true);
+  if (!validUrl) {
+    return callback(
       null,
-      success({
-        linkId: params.Item.linkId
+      failure({
+        message:
+          "Please supply an URL to be scraped in the url query parameter."
       })
     );
+  }
+  try {
+    const { body: html, url } = await got(data.url);
+    const metadata = await metascraper({ html, url });
+    // Return the retrieved item
+    callback(null, success(metadata));
   } catch (e) {
     console.error(e);
-    callback(null, failure({ status: false }));
+    callback(
+      null,
+      failure({
+        message: `Scraping the open graph data from ${url} failed.
+          \nsuggestion: 'Make sure your URL is correct and the webpage has open graph data, meta tags or twitter card data.`
+      })
+    );
   }
 }
