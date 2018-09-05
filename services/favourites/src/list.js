5a80374a83820cbc857702d88bcb2772e8317c0f
Richard <richard.dimalanta@gmail.com>
2018-09-05 21:22:52 +1000

    added favourites API

---
+++ services/favourites/src/list.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "../../../libs/dynamodb-lib";
+import { success, failure } from "../../../libs/response-lib";
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    FilterExpression:
+      "userId = :userId and isActive = :isActive and isFavourite = :isFavourite",
+    ProjectionExpression:
+      "linkId, author, description, image, logo, publisher, title, urlText, isFavourite",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId,
+      ":isFavourite": true,
+      ":isActive": true
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("scan", params);
+    // Return the matching list of items in response body
+    callback(null, success(result.Items));
+  } catch (e) {
+    console.error(e);
+    callback(null, failure({ status: false }));
+  }
+}
