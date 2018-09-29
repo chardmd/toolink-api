02268145c974521376d132c5f8364f1c343d8967
Richard <richard.dimalanta@gmail.com>
2018-09-30 09:42:09 +1000

    subscription plan

---
+++ services/billing/src/subscription.js
@@ -0,0 +1,24 @@
+import stripePackage from "stripe";
+import { success, failure } from "../../../libs/response-lib";
+
+export async function main(event, context, callback) {
+  let result = null;
+  try {
+    const data = JSON.parse(event.body);
+
+    // Load our secret key from the  environment variables
+    const stripe = stripePackage(process.env.stripeSecretKey);
+
+    result = await stripe.subscriptions.create({
+      customer: data.customer,
+      plan: data.planId
+    });
+
+    console.log({ result });
+
+    callback(null, success(result));
+  } catch (e) {
+    console.error(e);
+    callback(null, failure(result));
+  }
+}
