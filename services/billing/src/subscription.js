dd252a6b53b458033b78111c4e422495a8a4e6bc
Richard <richard.dimalanta@gmail.com>
2018-10-02 20:24:54 +1000

    Fixed issue on subscription

--- services/billing/src/subscription.js
+++ services/billing/src/subscription.js
@@ -11,7 +11,7 @@ export async function main(event, context, callback) {
 
     const customer = await stripe.customers.create({
       email: data.email,
-      source // this is used by stripe for creating credit card details
+      source: data.source // this is used by stripe for creating credit card details
     });
 
     result = await stripe.subscriptions.create({
